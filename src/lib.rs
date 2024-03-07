05177a162660cc5aa703a98934c7377e5ed9f42f
Iman Kazerani <kazeraniman@gmail.com>
2024-03-07 01:17:55 -0500

    Adding some helper methods to extract the necessary information from the opcode bytes to determine the actual opcode.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ use std::{io, fs};
 
 const CLEAR_SCREEN_OPCODE: [u8; 2] = [0x00, 0xE0];
 const RETURN_OPCODE: [u8; 2] = [0x00, 0xEE];
+const LOWER_NIBBLE_MASK: u8 = 0xF;
+const UPPER_NIBBLE_MASK: u8 = 0xF0;
 
 #[derive(PartialEq, Debug)]
 enum Opcode {
@@ -47,6 +49,7 @@ pub fn read_game_file(path: &str) -> io::Result<Vec<u8>> {
 }
 
 fn get_opcode(opcode_bytes: &[u8]) -> Opcode {
+    // TODO: Extract this as some struct so that this check does not need to be duplicated in various methods
     if opcode_bytes.len() != 2 {
         panic!("Opcodes must be two bytes.");
     }
@@ -59,7 +62,29 @@ fn get_opcode(opcode_bytes: &[u8]) -> Opcode {
         return Opcode::Return;
     }
 
-    panic!("Unrecognized opcode: {:0>2X?}{:0>2X?}", opcode_bytes[0], opcode_bytes[1])
+    let first_nibble = get_upper_nibble(opcode_bytes[0]);
+    let last_nibble = get_lower_nibble(opcode_bytes[1]);
+    let opcode_selection_info = (first_nibble, last_nibble, opcode_bytes[0], opcode_bytes[1]);
+    match opcode_selection_info {
+        (0x0, _, _, _) => Opcode::SystemAddr(get_addr(opcode_bytes)),
+        _ => panic!("Unrecognized opcode: {:0>2X?}{:0>2X?}", opcode_bytes[0], opcode_bytes[1])
+    }
+}
+
+fn get_upper_nibble(byte: u8) -> u8 {
+    (byte & UPPER_NIBBLE_MASK) >> 4
+}
+
+fn get_lower_nibble(byte: u8) -> u8 {
+    byte & LOWER_NIBBLE_MASK
+}
+
+fn get_addr(opcode_bytes: &[u8]) -> u16 {
+    if opcode_bytes.len() != 2 {
+        panic!("Opcodes must be two bytes.");
+    }
+
+    ((get_lower_nibble(opcode_bytes[0]) as u16) << 8) + (opcode_bytes[1] as u16)
 }
 
 #[cfg(test)]
@@ -77,6 +102,19 @@ mod tests {
     }
 
     #[test]
+    fn get_nibble() {
+        let byte = 0xAE;
+        assert_eq!(get_upper_nibble(byte), 0xA);
+        assert_eq!(get_lower_nibble(byte), 0xE);
+    }
+
+    #[test]
+    fn get_addr_value() {
+        let opcode = [0x8A, 0x78];
+        assert_eq!(get_addr(&opcode), 0xA78)
+    }
+
+    #[test]
     #[should_panic]
     fn get_improper_opcode() {
         let improper_opcode = [0x0];
@@ -86,21 +124,21 @@ mod tests {
     #[test]
     fn get_sys_addr_opcode() {
         let opcode = [0x0A, 0x78];
-        let opcode = get_opcode(&opcode[..]);
+        let opcode = get_opcode(&opcode);
         assert_eq!(opcode, Opcode::SystemAddr(0xA78));
     }
 
     #[test]
     fn get_clear_screen_opcode() {
         let opcode = CLEAR_SCREEN_OPCODE;
-        let opcode = get_opcode(&opcode[..]);
+        let opcode = get_opcode(&opcode);
         assert_eq!(opcode, Opcode::ClearScreen);
     }
 
     #[test]
     fn get_return_opcode() {
         let opcode = RETURN_OPCODE;
-        let opcode = get_opcode(&opcode[..]);
+        let opcode = get_opcode(&opcode);
         assert_eq!(opcode, Opcode::Return);
     }
 }
